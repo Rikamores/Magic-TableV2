fb0668bdcf9042ac8a5767d7f2570e934f5a3b97
Isabella Muñoz <142456520+Isa-Munoz@users.noreply.github.com>
2023-09-25 19:46:39 -0500

    Update header.php

--- vista/layout/header.php
+++ vista/layout/header.php
@@ -3,7 +3,6 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Magic Table - Restaurantes</title>
     <link rel="stylesheet" href="../asset/css/style.css">
     <link rel="icon" href="../asset/images/cubiertos.png" type="image/png">
 </head>
